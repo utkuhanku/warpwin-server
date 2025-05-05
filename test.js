70de314171ddb613dcd6ddded7ddf4da51dc3ad4
Utkuhan Kocaturk <utkuhank@hotmail.com>
2025-05-05 12:37:38 +0300

    test.js dosyası silindi

--- test.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.get('/', (req, res) => {
-  res.send('Merhaba, sunucu çalışıyor!');
-});
-
-app.listen(3000, () => console.log('Test sunucusu 3000 portunda çalışıyor'));
